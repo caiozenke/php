11b4bdc7c318532a061549bba89cf5b6f2c9a39d
caiozenke <74631392+caiozenke@users.noreply.github.com>
2022-11-30 11:31:36 -0300

    teste

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <title>Php</title>
+</head>
+<body>
+    <?php 
+        echo'caio';
+        print('oi')
+    ?>
+    
+</body>
+</html>
